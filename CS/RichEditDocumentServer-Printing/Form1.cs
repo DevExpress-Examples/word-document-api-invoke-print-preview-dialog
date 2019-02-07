73d788580f525e6031852a12a113b693445a040b
Poline Fedorova <polina.fedorova@devexpress.com>
2019-02-07 12:53:26 +0300

    Add files via upload

--- CS/RichEditDocumentServer-Printing/Form1.cs
+++ CS/RichEditDocumentServer-Printing/Form1.cs
@@ -20,35 +20,27 @@ namespace RichEditDocumentServer_Printing
             InitializeComponent();
         }
 
-private void btn_Print_Click(object sender, EventArgs e)
-{
-#region #serverprint
-    RichEditDocumentServer richServer = new RichEditDocumentServer();
-    // Specify default formatting
-    richServer.Document.DefaultParagraphProperties.Alignment = ParagraphAlignment.Center;
-    // Specify page settings
-    richServer.Document.Sections[0].Page.Landscape = true;
-    richServer.Document.Sections[0].Page.Height = DevExpress.Office.Utils.Units.InchesToDocumentsF(10.0f);
-    richServer.Document.Sections[0].Page.Width = DevExpress.Office.Utils.Units.InchesToDocumentsF(4.5f);
-    // Add document content
-    richServer.Document.AppendText("This content is created programmatically\n");
-    richServer.Document.AppendParagraph();
-    InsertTableIntoDocument(richServer);
-    // Invoke the Print Preview dialog
-    using (PrintingSystem printingSystem = new PrintingSystem()) {
-        using (PrintableComponentLink link = new PrintableComponentLink(printingSystem)) {
-            link.Component = richServer;
-            link.CreateDocument();
-            link.ShowPreviewDialog();
-        }
-    }
-#endregion #serverprint
-}
-        private static void InsertTableIntoDocument(RichEditDocumentServer richServer)
+        private void btn_Print_Click(object sender, EventArgs e)
         {
+            #region #serverprint
+            RichEditDocumentServer richServer = new RichEditDocumentServer();
+
+            // Specify default formatting
+            richServer.Document.DefaultParagraphProperties.Alignment = ParagraphAlignment.Center;
+
+            // Specify page settings
+            richServer.Document.Sections[0].Page.Landscape = true;
+            richServer.Document.Sections[0].Page.Height = DevExpress.Office.Utils.Units.InchesToDocumentsF(10.0f);
+            richServer.Document.Sections[0].Page.Width = DevExpress.Office.Utils.Units.InchesToDocumentsF(4.5f);
+
+            // Add document content
+            richServer.Document.AppendText("This content is created programmatically\n");
+            richServer.Document.Paragraphs.Append();
+
+            //Create a table
             richServer.BeginUpdate();
-            
-            Table _table = richServer.Document.Tables.Add(richServer.Document.Selection.Start, 8, 8, AutoFitBehaviorType.FixedColumnWidth);
+
+            Table _table = richServer.Document.Tables.Create(richServer.Document.Selection.Start, 8, 8, AutoFitBehaviorType.FixedColumnWidth);
             _table.BeginUpdate();
             _table.Borders.InsideHorizontalBorder.LineThickness = 1;
             _table.Borders.InsideHorizontalBorder.LineStyle = TableBorderLineStyle.Double;
@@ -56,11 +48,26 @@ private void btn_Print_Click(object sender, EventArgs e)
             _table.Borders.InsideVerticalBorder.LineStyle = TableBorderLineStyle.Double;
             _table.TableAlignment = TableRowAlignment.Center;
 
-            _table.ForEachCell(new TableCellProcessorDelegate(delegate(TableCell cell, int i, int j) 
-            { richServer.Document.InsertText(cell.Range.Start, String.Format("{0}*{1} is {2}", i+2, j+2, (i+2) * (j+2))); }));
+            _table.ForEachCell((cell, rowIndex, columnIndex) =>
+            {
+                richServer.Document.InsertText(cell.Range.Start, String.Format("{0}*{1} is {2}",
+                    rowIndex + 2, columnIndex + 2, (rowIndex + 2) * (columnIndex + 2)));
+            });
             _table.EndUpdate();
 
             richServer.EndUpdate();
+
+            // Invoke the Print Preview dialog
+            using (PrintingSystem printingSystem = new PrintingSystem())
+            {
+                using (PrintableComponentLink link = new PrintableComponentLink(printingSystem))
+                {
+                    link.Component = richServer;
+                    link.CreateDocument();
+                    link.ShowPreviewDialog();
+                }
+            }
+            #endregion #serverprint
         }
     }
 }
--- VB/RichEditDocumentServer-Printing/Form1.vb
+++ VB/RichEditDocumentServer-Printing/Form1.vb
@@ -13,55 +13,53 @@ Imports DevExpress.XtraPrinting
 #End Region ' #usings
 
 Namespace RichEditDocumentServer_Printing
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class Form1
+        Inherits Form
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-Private Sub btn_Print_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btn_Print.Click
-'#Region "#serverprint"
-	Dim richServer As New RichEditDocumentServer()
-	' Specify default formatting
-	richServer.Document.DefaultParagraphProperties.Alignment = ParagraphAlignment.Center
-	' Specify page settings
-	richServer.Document.Sections(0).Page.Landscape = True
-	richServer.Document.Sections(0).Page.Height = DevExpress.Office.Utils.Units.InchesToDocumentsF(10.0f)
-	richServer.Document.Sections(0).Page.Width = DevExpress.Office.Utils.Units.InchesToDocumentsF(4.5f)
-	' Add document content
-	richServer.Document.AppendText("This content is created programmatically" & Constants.vbLf)
-	richServer.Document.AppendParagraph()
-	InsertTableIntoDocument(richServer)
-	' Invoke the Print Preview dialog
-	Using printingSystem As New PrintingSystem()
-		Using link As New PrintableComponentLink(printingSystem)
-			link.Component = richServer
-			link.CreateDocument()
-			link.ShowPreviewDialog()
-		End Using
-	End Using
-'#End Region ' #serverprint
-End Sub
-		Private Shared Sub InsertTableIntoDocument(ByVal richServer As RichEditDocumentServer)
-			richServer.BeginUpdate()
+        Private Sub btn_Print_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btn_Print.Click
+            '#Region "#serverprint"
+            Dim richServer As New RichEditDocumentServer()
+            ' Specify default formatting
+            richServer.Document.DefaultParagraphProperties.Alignment = ParagraphAlignment.Center
+            ' Specify page settings
+            richServer.Document.Sections(0).Page.Landscape = True
+            richServer.Document.Sections(0).Page.Height = DevExpress.Office.Utils.Units.InchesToDocumentsF(10.0F)
+            richServer.Document.Sections(0).Page.Width = DevExpress.Office.Utils.Units.InchesToDocumentsF(4.5F)
+            ' Add document content
+            richServer.Document.AppendText("This content is created programmatically" & Constants.vbLf)
+            richServer.Document.Paragraphs.Append()
 
-			Dim _table As Table = richServer.Document.Tables.Add(richServer.Document.Selection.Start, 8, 8, AutoFitBehaviorType.FixedColumnWidth)
-			_table.BeginUpdate()
-			_table.Borders.InsideHorizontalBorder.LineThickness = 1
-			_table.Borders.InsideHorizontalBorder.LineStyle = TableBorderLineStyle.Double
-			_table.Borders.InsideVerticalBorder.LineThickness = 1
-			_table.Borders.InsideVerticalBorder.LineStyle = TableBorderLineStyle.Double
-			_table.TableAlignment = TableRowAlignment.Center
+            ' Create a table
+            richServer.BeginUpdate()
 
-			_table.ForEachCell(New TableCellProcessorDelegate(Function(cell, i, j) AnonymousMethod1(cell, i, j, richServer)))
-			_table.EndUpdate()
+            Dim _table As Table = richServer.Document.Tables.Create(richServer.Document.Selection.Start, 8, 8, AutoFitBehaviorType.FixedColumnWidth)
+            _table.BeginUpdate()
+            _table.Borders.InsideHorizontalBorder.LineThickness = 1
+            _table.Borders.InsideHorizontalBorder.LineStyle = TableBorderLineStyle.Double
+            _table.Borders.InsideVerticalBorder.LineThickness = 1
+            _table.Borders.InsideVerticalBorder.LineStyle = TableBorderLineStyle.Double
+            _table.TableAlignment = TableRowAlignment.Center
 
-			richServer.EndUpdate()
-		End Sub
-		
-		Private Shared Function AnonymousMethod1(ByVal cell As TableCell, ByVal i As Integer, ByVal j As Integer, ByVal richServer As RichEditDocumentServer) As Boolean
-			richServer.Document.InsertText(cell.Range.Start, String.Format("{0}*{1} is {2}", i+2, j+2, (i+2) * (j+2)))
-			Return True
-		End Function
-	End Class
+            _table.ForEachCell(Sub(cell, rowIndex, columnIndex)
+                                   richServer.Document.InsertText(cell.Range.Start, String.Format("{0}*{1} is {2}",
+                                                                rowIndex + 2, columnIndex + 2, (rowIndex + 2) * (columnIndex + 2)))
+                               End Sub)
+            _table.EndUpdate()
+
+            richServer.EndUpdate()
+
+            ' Invoke the Print Preview dialog
+            Using printingSystem As New PrintingSystem()
+                Using link As New PrintableComponentLink(printingSystem)
+                    link.Component = richServer
+                    link.CreateDocument()
+                    link.ShowPreviewDialog()
+                End Using
+            End Using
+            '#End Region ' #serverprint
+        End Sub
+    End Class
 End Namespace
