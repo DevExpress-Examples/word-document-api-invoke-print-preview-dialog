ee0078bf51d7cfe1501d816589ac828753cee151
DevExpressExampleBot <fakeEmail>
2022-01-27 15:48:21 +0300

    Source auto update [skip ci]

--- VB/RichEditDocumentServer-Printing/Form1.vb
+++ VB/RichEditDocumentServer-Printing/Form1.vb
@@ -1,27 +1,25 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 #Region "#usings"
 Imports DevExpress.XtraRichEdit
 Imports DevExpress.XtraRichEdit.API.Native
 Imports DevExpress.XtraPrinting
-#End Region ' #usings
 
+#End Region  ' #usings
 Namespace RichEditDocumentServer_Printing
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
+
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub btn_Print_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btn_Print.Click
-            '#Region "#serverprint"
-            Dim richServer As New RichEditDocumentServer()
+        Private Sub btn_Print_Click(ByVal sender As Object, ByVal e As EventArgs)
+#Region "#serverprint"
+            Dim richServer As RichEditDocumentServer = New RichEditDocumentServer()
             ' Specify default formatting
             richServer.Document.DefaultParagraphProperties.Alignment = ParagraphAlignment.Center
             ' Specify page settings
@@ -29,12 +27,10 @@ Namespace RichEditDocumentServer_Printing
             richServer.Document.Sections(0).Page.Height = DevExpress.Office.Utils.Units.InchesToDocumentsF(10.0F)
             richServer.Document.Sections(0).Page.Width = DevExpress.Office.Utils.Units.InchesToDocumentsF(4.5F)
             ' Add document content
-            richServer.Document.AppendText("This content is created programmatically" & Constants.vbLf)
+            richServer.Document.AppendText("This content is created programmatically" & Microsoft.VisualBasic.Constants.vbLf)
             richServer.Document.Paragraphs.Append()
-
-            ' Create a table
+            'Create a table
             richServer.BeginUpdate()
-
             Dim _table As Table = richServer.Document.Tables.Create(richServer.Document.Selection.Start, 8, 8, AutoFitBehaviorType.FixedColumnWidth)
             _table.BeginUpdate()
             _table.Borders.InsideHorizontalBorder.LineThickness = 1
@@ -42,24 +38,18 @@ Namespace RichEditDocumentServer_Printing
             _table.Borders.InsideVerticalBorder.LineThickness = 1
             _table.Borders.InsideVerticalBorder.LineStyle = TableBorderLineStyle.Double
             _table.TableAlignment = TableRowAlignment.Center
-
-            _table.ForEachCell(Sub(cell, rowIndex, columnIndex)
-                                   richServer.Document.InsertText(cell.Range.Start, String.Format("{0}*{1} is {2}",
-                                                                rowIndex + 2, columnIndex + 2, (rowIndex + 2) * (columnIndex + 2)))
-                               End Sub)
+            _table.ForEachCell(Sub(cell, rowIndex, columnIndex) richServer.Document.InsertText(cell.Range.Start, String.Format("{0}*{1} is {2}", rowIndex + 2, columnIndex + 2, (rowIndex + 2) * (columnIndex + 2))))
             _table.EndUpdate()
-
             richServer.EndUpdate()
-
             ' Invoke the Print Preview dialog
-            Using printingSystem As New PrintingSystem()
-                Using link As New PrintableComponentLink(printingSystem)
+            Using printingSystem As PrintingSystem = New PrintingSystem()
+                Using link As PrintableComponentLink = New PrintableComponentLink(printingSystem)
                     link.Component = richServer
                     link.CreateDocument()
                     link.ShowPreviewDialog()
                 End Using
             End Using
-            '#End Region ' #serverprint
+#End Region  ' #serverprint
         End Sub
     End Class
 End Namespace
